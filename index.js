e6e826e7a3be690439bcc270c414b4ca96dde601
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-14 14:07:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class Triangle extends Polygon {
     let side2 = this.sides[1]
     let side3 = this.sides[2]
 
-    if((this.sides.length === 3) && (side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1)) return
+    if((this.sides.length === 3) && (side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1)) return true;
     else {
       return false;
     }
